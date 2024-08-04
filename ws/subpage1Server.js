f02fef7d12a6976174b6cf8dbcc96d47c038a376
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-04 12:54:36 +0200

    Update subpage1Server.js

--- ws/subpage1Server.js
+++ ws/subpage1Server.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "https://separate-ws-servers.onrender.com", // Allow requests from the main server
+        origin: "*", // Allow requests from the main server
         methods: ["GET", "POST"]
     }
 });
