ab88e87e23a961644393214acb17dde195458319
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-04 15:27:18 +0200

    Update subpage1Server.js

--- ws/subpage1Server.js
+++ ws/subpage1Server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3001; // Use the PORT environment variable set by Render
+const port = 3001; // Ensure this is the correct port for Render service
 
 app.use(cors({
-    origins:"https://separate-ws-servers.onrender.com/subpage1", // Your frontend's URL
+    origin: "https://separate-ws-servers.onrender.com",
     methods: ["GET", "POST"],
     credentials: true
 }));
@@ -23,18 +23,15 @@ const io = socketIo(server, {
 
 io.on('connection', (socket) => {
     console.log('User connected to Subpage 1 WebSocket server');
-
     socket.on('message', (msg) => {
         console.log(`Message from client on Subpage 1: ${msg}`);
     });
-
     socket.send('Welcome to the WebSocket server for Subpage 1');
-
     socket.on('disconnect', () => {
         console.log('User disconnected from Subpage 1 WebSocket server');
     });
 });
 
 server.listen(port, () => {
-    console.log(`Subpage 1 WebSocket server is running on http://localhost:${port}`);
+    console.log(`Subpage 1 WebSocket server is running on port ${port}`);
 });
