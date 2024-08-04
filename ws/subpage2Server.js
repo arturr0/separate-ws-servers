4d40cedf9faae315e9094c38b98c51d287c161c1
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-04 13:57:18 +0200

    Update subpage2Server.js

--- ws/subpage2Server.js
+++ ws/subpage2Server.js
@@ -4,32 +4,34 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3002; // Port for Subpage 2 WebSocket server
+const port = 3002; // Port for Subpage 1
 
-app.use(cors());
+app.use(cors()); // Enable CORS
 
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+        origin: "https://separate-ws-servers.onrender.com", // Allowed origin
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
     }
 });
 
 io.on('connection', (socket) => {
-    console.log('User connected to Subpage 2 WebSocket server');
+    console.log('User connected to Subpage 1 WebSocket server');
 
     socket.on('message', (msg) => {
-        console.log(`Message from client on Subpage 2: ${msg}`);
+        console.log(`Message from client on Subpage 1: ${msg}`);
     });
 
-    socket.send('Welcome to the WebSocket server for Subpage 2');
+    socket.send('Welcome to the WebSocket server for Subpage 1');
 
     socket.on('disconnect', () => {
-        console.log('User disconnected from Subpage 2 WebSocket server');
+        console.log('User disconnected from Subpage 1 WebSocket server');
     });
 });
 
 server.listen(port, () => {
-    console.log(`Subpage 2 WebSocket server is running on http://localhost:${port}`);
+    console.log(`Subpage 1 WebSocket server is running on http://localhost:${port}`);
 });
