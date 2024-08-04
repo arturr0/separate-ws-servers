107cf6a2cf3a8f984332060a973fa52e2339da9d
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-04 15:26:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes for different pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/home.html'));
 });
