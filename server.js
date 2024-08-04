6c401210034e1c5a069936c3b44b1c1f1aea1146
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-04 15:34:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/home.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/subpage1', (req, res) => {
