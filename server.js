ddaa67b24b56446e5b91c30e4d7f4ac7b8fb33c7
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-04 13:30:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
-const socketIo = require('socket.io');
-const cors = require('cors');
-
-// Import WebSocket handlers
-const subpage1Ws = require('./ws/subpage1Server');
-const subpage2Ws = require('./ws/subpage2Server');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
-const server = http.createServer(app);
-const io = socketIo(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
-
-// Attach WebSocket handlers to namespaces
-io.of('/subpage1').on('connection', subpage1Ws);
-io.of('/subpage2').on('connection', subpage2Ws);
 
 // Define routes for different pages
 app.get('/', (req, res) => {
@@ -39,6 +19,6 @@ app.get('/subpage2', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/subpage2.html'));
 });
 
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`HTTP server is running on http://localhost:${port}`);
 });
