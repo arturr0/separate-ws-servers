a30f81f2e44376981300ac930c519130789e1d1b
arturr0 <119508496+arturr0@users.noreply.github.com>
2024-08-05 17:58:47 +0200

    Update subpage2Server.js

--- ws/subpage2Server.js
+++ ws/subpage2Server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3001; // Port for the WebSocket server
+const port = 3002; // Port for the WebSocket server
 
 // Replace "http://your-client-domain.com" with your actual client's domain
 app.use(cors({
@@ -25,24 +25,24 @@ const io = socketIo(server, {
 
 // Handle WebSocket connections
 io.on('connection', (socket) => {
-    console.log('User connected to Subpage 1 WebSocket server');
+    console.log('User connected to Subpage 2 WebSocket server');
 
     socket.on('message', (msg) => {
-        console.log(`Message from client on Subpage 1: ${msg}`);
+        console.log(`Message from client on Subpage 2: ${msg}`);
     });
 
     // Send a welcome message to the client
-    socket.send('Welcome to the WebSocket server for Subpage 1');
+    socket.send('Welcome to the WebSocket server for Subpage 2');
 
     // Handle client disconnection
     socket.on('disconnect', () => {
-        console.log('User disconnected from Subpage 1 WebSocket server');
+        console.log('User disconnected from Subpage 2 WebSocket server');
     });
 });
 
 // Start the server
 server.listen(port, () => {
-    console.log(`Subpage 1 WebSocket server is running on port ${port}`);
+    console.log(`Subpage 2 WebSocket server is running on port ${port}`);
 });
 
 // Error handling for server issues
